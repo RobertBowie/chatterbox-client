e1ea9d11a5ad62b1e433dda61cfd173ac891b39e
RobertBowie <robertb1903@gmail.com>
2015-10-19 17:17:08 -0700

    Pulls posts reverse chronologically.

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,2 +1,33 @@
 // YOUR CODE HERE:
+var app = {};
+var messages;
+$.ajax({
+  // This is the url you should use to communicate with the parse API server.
+  url: 'https://api.parse.com/1/classes/chatterbox?order=-createdAt',
+  type: 'GET',
+  contentType: 'application/json',
+  success: function (data) {
+    messages = data.results.reverse();
+    console.log('chatterbox: Message: Data: ', data);
+    printMessages();
+  },
+  error: function (data) {
+    // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
+    console.error('chatterbox: Failed to get message. Error: ', data);
+  }
+});
 
+function printMessages () { 
+  for (var i = 0; i < 10; i++) {
+    console.log(messages[i]);
+  }
+  console.log('Data: ' , messages);
+  messages.forEach(function(message) {
+    $('#main').append('<div class="chat">' + message.username + ': ' + (message.text || message.message) + '\n' + 
+    message.updatedAt + '</div>');
+  });
+}
+
+function messageFormatter () {
+
+}
