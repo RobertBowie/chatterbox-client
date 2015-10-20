6c5db21f0b22fe7ede35b22881c33f636225f28e
RobertBowie <robertb1903@gmail.com>
2015-10-19 20:02:54 -0700

    Added work up to day end.

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,33 +1,60 @@
 // YOUR CODE HERE:
 var app = {};
-var messages;
-$.ajax({
-  // This is the url you should use to communicate with the parse API server.
-  url: 'https://api.parse.com/1/classes/chatterbox?order=-createdAt',
-  type: 'GET',
-  contentType: 'application/json',
-  success: function (data) {
-    messages = data.results.reverse();
-    console.log('chatterbox: Message: Data: ', data);
-    printMessages();
-  },
-  error: function (data) {
-    // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
-    console.error('chatterbox: Failed to get message. Error: ', data);
-  }
-});
+app.messages;
+app.server = 'https://api.parse.com/1/classes/chatterbox';
+app.init = function() {
+  app.fetch();
+};
+//Fetch can be for new data
 
-function printMessages () { 
-  for (var i = 0; i < 10; i++) {
-    console.log(messages[i]);
-  }
-  console.log('Data: ' , messages);
-  messages.forEach(function(message) {
-    $('#main').append('<div class="chat">' + message.username + ': ' + (message.text || message.message) + '\n' + 
-    message.updatedAt + '</div>');
+app.send = function(message) {
+  $.ajax({
+    url: app.server,
+    type: 'POST',
+    data: JSON.stringify(message),
+    contentType: 'application/json',
+    success: function (data) {
+      console.log('chatterbox: Message sent. Data: ', data);
+    },
+    error: function (data) {
+      // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
+      console.error('chatterbox: Failed to send message. Error: ', data);
+    }
   });
-}
+};
 
-function messageFormatter () {
+app.fetch = function() {
+    $.ajax({
+    // This is the url you should use to communicate with the parse API server.
+    url: app.server + '?order=-createdAt',
+    type: 'GET',
+    contentType: 'application/json',
+    success: function (data) {
+      app.messages = data.results.reverse();
+      console.log('chatterbox: Message: Data: ', data);
+      app.addMessage();
+    },
+    error: function (data) {
+      // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
+      console.error('chatterbox: Failed to get message. Error: ', data);
+    }
+  });
+};
 
-}
+app.addMessage = function() { 
+  app.messages.forEach(function(message) {
+    if (message.text && message.text.charAt(0) !== '<') {
+    console.log('Data: ' , message);
+    var messageText = message.username + ' ' + message.text;
+    $('#main').append('<div class="chat">' + messageText + '</div>');
+    }
+  });
+};
+
+app.init();
+// function messageFormatter (message) {
+   //  message.username.toString() + ': "'
+   // + (if(message.text){message.text.toString()} || message.message.toString())
+   // + '"'
+   // + message.updatedAt
+// }
